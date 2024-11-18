141200557c6b3b27f41db6a8f6a164f4ad8dbd78
jus1d <artfa63@gmail.com>
2024-11-19 00:37:06 +0400

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://ndbtea.xyz',
+	site: 'https://ndbtea.tech',
 	integrations: [
 		expressiveCode(expressiveCodeOptions),
 		tailwind({
